0b5a84e962a1b50b70b7ce3dc90fe4390d311fc6
bgulbis <brian.gulbis@gmail.com>
2016-01-18 10:31:23 -0600

    update ccs data

--- data-raw/ccs_diagnosis.R
+++ data-raw/ccs_diagnosis.R
@@ -5,6 +5,7 @@ library(devtools)
 
 ccs.diagnosis <- read.csv("data-raw/icd9_ccs_diagnosis.csv", colClasses="character") %>%
     transmute(ccs.code = as.numeric(CCS.CATEGORY),
+              ccs.description = CCS.CATEGORY.DESCRIPTION,
               icd9.code = ICD9.CODE.FRMT,
               icd9.description = ICD.9.CM.CODE.DESCRIPTION)
 
