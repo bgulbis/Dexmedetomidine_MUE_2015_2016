ae01b847ec604af30f8b1444bd27bf580061fa26
bgulbis <brian.gulbis@gmail.com>
2016-02-07 10:38:26 -0600

    begin apache calculator

---
+++ R/apache_score.R
@@ -0,0 +1,21 @@
+# apache_score.R
+
+#' Calculate APACHE-II score
+#'
+#' \code{apache_score} takes a data frame with patient data and calculates the
+#' APACHE II score
+#'
+#' This function takes a data frame containing all of the data needed to
+#' calculate the APAPCHE II score.
+#'
+#' @param x A data frame
+#'
+#' @return A data frame with all original columns plus APACHE II score
+#'
+#' @import dplyr
+#'
+#' @export
+apache_score <- function(x) {
+
+
+}
