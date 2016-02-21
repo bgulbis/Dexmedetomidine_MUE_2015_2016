f70f097f4eb0f9d2b268123c1e4fd292f023a7cf
bgulbis <brian.gulbis@gmail.com>
2016-02-21 12:37:35 -0600

    finished standard eval rewrite

--- R/icd9_lookup.R
+++ R/icd9_lookup.R
@@ -22,8 +22,6 @@
 #'
 #' @return A data frame with columns: disease.state, icd9.code
 #'
-#' @import dplyr
-#'
 #' @export
 icd9_lookup <- function(df, procedure = FALSE) {
     if (procedure == TRUE) {
@@ -33,19 +31,25 @@ icd9_lookup <- function(df, procedure = FALSE) {
     }
 
     # find the ICD9 codes for the desired exclusions by CCS code
-    ccs <- filter(df, type == "CCS") %>%
-        mutate(ccs.code = as.numeric(code)) %>%
-        inner_join(data, by="ccs.code")
+    ccs <- dplyr::filter_(df, .dots = list(~type == "CCS"))
+    # make all CCS codes numeric
+    ccs <- dplyr::mutate_(ccs, .dots = list(~as.numeric(code)))
+    # join CCS list with data
+    ccs <- dplyr::inner_join(ccs, data, by = "ccs.code")
 
     # ICD9 codes for non-CCS code exclusions
-    icd9 <- filter(df, type=="ICD9") %>%
-        mutate(icd9.code = code) %>%
-        inner_join(data, by="icd9.code")
+    icd9 <- dplyr::filter_(df, .dots = list(~type == "ICD9"))
+    # rename code column
+    icd9 <- dplyr::rename_(icd9, .dots = setNames("code", "icd9.code"))
+    # join list with data
+    icd9 <- dplyr::inner_join(icd9, data, by = "icd9.code")
 
     # create one table with all ICD9 codes that should be excluded
-    codes <- bind_rows(ccs, icd9) %>%
-        select(disease.state, icd9.code) %>%
-        group_by(disease.state)
+    codes <- dplyr::bind_rows(ccs, icd9)
+    # keep only the disease state and ICD9 code
+    codes <- dplyr::select_(codes, .dots = list("disease.state", "icd9.code"))
+    # group by disease state
+    codes <- dplyr::group_by_(codes, "disease.state")
 
     return(codes)
 }
@@ -66,8 +70,6 @@ icd9_lookup <- function(df, procedure = FALSE) {
 #'
 #' @return A data frame with columns: icd9.code and icd9.description
 #'
-#' @import dplyr
-#'
 #' @export
 icd9_description <- function(codes, procedure = FALSE) {
     if (procedure == TRUE) {
@@ -76,9 +78,11 @@ icd9_description <- function(codes, procedure = FALSE) {
         data <- ccs.diagnosis
     }
 
-    descript <- data %>%
-        filter(icd9.code %in% codes) %>%
-        select(icd9.code, icd9.description)
+    # get only the desired descriptions based on ICD9 codes
+    descript <- dplyr::filter_(data, .dots = list(~icd9.code %in% codes))
+    # keep only ICD9 code and description columns
+    dots <- list("icd9.code", "icd9.description")
+    descript <- dplyr::select_(descript, .dots = dots)
 
     return(descript)
 }
--- R/med_classes.R
+++ R/med_classes.R
@@ -16,39 +16,43 @@
 #'
 #' @return A data frame with columns: med.class and med.name
 #'
-#' @import dplyr
-#'
 #' @export
 med_lookup <- function(med_class) {
-    meds <- med.classes %>%
-        filter(med.class %in% med_class)
+    # filter med.classes to only the desired classes
+    dots <- list(~med.class %in% med_class)
+    meds <- dplyr::filter_(med.classes, .dots = dots)
 
     return(meds)
 }
 
 #' Lookup medication class by medication generic name
 #'
-#' \code{med_class_lookup} takes a vector of medication generic names and returns the
-#' classes which contain those medications
+#' \code{med_class_lookup} takes a vector of medication generic names and
+#' returns the classes which contain those medications
 #'
-#' This function takes a character vector of medication generic names and returns a data
-#' frame with all of the medication classes that contain those medications. The
-#' data frame will contain two columns: med.class and med.name.
+#' This function takes a character vector of medication generic names and
+#' returns a data frame with all of the medication classes that contain those
+#' medications. The data frame will contain two columns: med.class and med.name.
 #'
 #' @param meds A character vector of medication names
 #'
 #' @return A data frame with columns: med.class and med.name
 #'
-#' @import dplyr
-#'
 #' @export
 med_class_lookup <- function(meds) {
+    # make a regular expression string of desired meds
     lookup <- paste(meds, collapse = "|")
 
-    meds <- med.classes %>%
-        mutate(contains = stringr::str_detect(med.name, stringr::regex(lookup, ignore_case = TRUE))) %>%
-        filter(contains == TRUE) %>%
-        select(-contains)
+    # check each medication to see if it is in the list of desired meds
+    regex <- stringr::regex(lookup, ignore_case = TRUE)
+    dots <- list(~stringr::str_detect(med.name, regex))
+    meds <- dplyr::mutate_(med.classes, .dots = setNames(dots, "contains"))
+
+    # filter only medications which match
+    meds <- dplyr::filter_(meds, .dots = list(~contains == TRUE))
+
+    # remove contains from data frame
+    meds <- dplyr::select_(meds, .dots = list(quote(-contains)))
 
     return(meds)
 }
