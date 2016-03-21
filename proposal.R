df88357594b5dd20460316848c4e35b73cf1172d
bgulbis <brian.gulbis@gmail.com>
2016-03-20 22:01:49 -0500

    use read_edw_data

--- proposal.R
+++ proposal.R
@@ -5,16 +5,9 @@
 source("library.R")
 
 # get list of patients from charge data
-dexmed <- read_data(proposal.dir, "patients") %>%
-    transmute(pie.id = PowerInsight.Encounter.Id,
-              cdm = Cdm.Code,
-              cdm.desc = factor(Cdm.Desc),
-              service.date = ymd_hms(Service.Date),
-              inst.code = factor(Institution.Code),
-              inst.name = factor(Institution.Desc)) %>%
-    distinct(pie.id) 
+dexmed <- read_edw_data(proposal.dir, "patients", "charges") %>%
+    distinct(pie.id)
 
 # make list of encounters for EDW
 edw.pie <- concat_encounters(dexmed$pie.id, 500)
 print(edw.pie)
-
