d80c212d370a2d862e07d3c275b3f8e496bc5529
bgulbis <brian.gulbis@gmail.com>
2016-04-14 10:43:28 -0500

    move directories to a separate script

--- 0-library.R
+++ 0-library.R
@@ -5,10 +5,8 @@ library(BGTools)
 library(stringr)
 library(lubridate)
 
-proposal.dir <- "proposal_data"
-screen.dir <- "screen_data"
-data.dir <- "data"
+source("0-dirs.R")
 
-gzip_files(proposal.dir)
-gzip_files(screen.dir)
-gzip_files(data.dir)
+gzip_files(dir.proposal)
+gzip_files(dir.screen)
+gzip_files(dir.data)
