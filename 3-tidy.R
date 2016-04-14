1bd316d4a24869a1b0c4383c22b7b7e939cf317f
bgulbis <brian.gulbis@gmail.com>
2016-04-14 12:31:34 -0500

    remove pedi patients; get height and weight

--- 3-tidy.R
+++ 3-tidy.R
@@ -17,6 +17,13 @@ data.locations <- read_edw_data(dir.data, "locations") %>%
     semi_join(data.demographics, by = "pie.id") %>%
     tidy_data("locations")
 
+# remove any patients who were in PICU
+tmp.pedi <- data.locations %>%
+    filter(str_detect(location, regex("children|pediatric", ignore_case = TRUE))) %>%
+    distinct(pie.id)
+
+data.demographics <- anti_join(data.demographics, tmp.pedi, by = "pie.id")
+
 # get dexmedetomidine data 
 cont.meds <- c("dexmedetomidine", "lorazepam", "midazolam", "propofol", 
                "ketamine", "fentanyl", "hydromorphone", "morphine")
@@ -24,7 +31,7 @@ ref.cont.meds <- data_frame(name = cont.meds, type = "med", group = "cont")
 
 raw.meds.sched <- read_edw_data(dir.data, "meds_sched")
 
-tmp.meds.cont <- read_edw_data(dir.data, "meds_continuous") %>%
+tmp.meds.cont <- read_edw_data(dir.data, "meds_continuous", check.distinct = FALSE) %>%
     tidy_data("meds_cont", ref.data = ref.cont.meds, sched.data = raw.meds.sched) 
 
 # get dexmed infusion information, separate into distinct infusions if off for >
@@ -45,6 +52,10 @@ data.meds.cont.sum <- data.meds.cont %>%
               cum.run.time = sum(run.time, na.rm = TRUE),
               time.wt.avg = sum(auc, na.rm = TRUE) / sum(duration, na.rm = TRUE))
 
+# identify which units patients were in while on dexmedetomidine
+tmp.dexmed <- select(data.meds.cont, pie.id:stop.datetime) %>%
+    filter(med == "dexmedetomidine") 
+
 data.demographics <- semi_join(data.demographics, data.meds.cont.sum, by = "pie.id")
 
 data.demographics <- semi_join(data.demographics, tmp.dexmed, by = "pie.id")
@@ -53,20 +64,43 @@ data.meds.cont <- semi_join(data.meds.cont, data.demographics, by = "pie.id")
 
 data.locations <- semi_join(data.locations, data.demographics, by = "pie.id")
 
-# identify which units patients were in while on dexmedetomidine
-tmp.dexmed <- select(data.meds.cont, pie.id:stop.datetime) %>%
-    filter(med == "dexmedetomidine") 
-
-data.locations.dexmed <- tmp.dexmed %>%
+data.dexmed <- tmp.dexmed %>%
+    semi_join(data.demographics, by = "pie.id") %>%
     rowwise %>%
     mutate(location = lookup_location(pie.id, start.datetime)) %>%
     ungroup
-    
+
+# get data for first dexmed course
+tmp.dexmed.first <- group_by(data.dexmed, pie.id) %>%
+    filter(drip.count == min(drip.count))
 
 # get raw data for all eligible patients
-# raw.measures <- read_edw_data(dir.data, "ht_wt", "measures")
+raw.measures <- read_edw_data(dir.data, "measures")
+
+tmp.height <- filter(raw.measures, measure == "Height",
+                     measure.units == "cm") %>%
+    semi_join(data.demographics, by = "pie.id") %>%
+    group_by(pie.id) %>%
+    summarize(height = first(measure.result))
+
+data.demographics <- inner_join(data.demographics, tmp.height, by = "pie.id")
+
+# a few patients didn't have a weight when dexmed was started, but had one
+# recorded within 8 hours
+tmp.weight <- filter(raw.measures, measure == "Weight",
+                     measure.units == "kg") %>%
+    semi_join(data.demographics, by = "pie.id") %>%
+    inner_join(tmp.dexmed.first, by = "pie.id") %>%
+    filter(measure.datetime <= start.datetime + hours(8)) %>%
+    group_by(pie.id) %>%
+    summarize(weight = last(measure.result))
+
+data.demographics <- left_join(data.demographics, tmp.weight, by = "pie.id")
+
 # raw.labs <- read_edw_data(dir.data, "labs")
 # raw.icu.assess <- read_edw_data(dir.data, "icu_assess")
 # raw.vent <- read_edw_data(dir.data, "vent")
 # raw.vitals <- read_edw_data(dir.data, "vitals")
 # raw.uop <- read_edw_data(dir.data, "uop")
+
+save_rds(dir.save, "^data")
