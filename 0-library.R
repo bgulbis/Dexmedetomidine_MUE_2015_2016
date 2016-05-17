dfb475f7edcf77a775cfd4baa4bfee4ce1a7e4a1
bgulbis <brian.gulbis@gmail.com>
2016-05-17 13:48:24 -0500

    do not use lookup_location function

--- 0-library.R
+++ 0-library.R
@@ -12,14 +12,14 @@ gzip_files(dir.proposal)
 gzip_files(dir.screen)
 gzip_files(dir.data)
 
-lookup_location <- function(pt, start) {
-    x <- filter(data.locations, pie.id == pt,
-                start >= arrive.datetime,
-                start <= depart.datetime)
-    
-    if (length(x$location) < 1) {
-        "Unable to match location"
-    } else {
-        x$location
-    }
-}
+# lookup_location <- function(pt, start) {
+#     x <- filter(data.locations, pie.id == pt,
+#                 start >= arrive.datetime,
+#                 start <= depart.datetime)
+#     
+#     if (length(x$location) < 1) {
+#         "Unable to match location"
+#     } else {
+#         x$location
+#     }
+# }
