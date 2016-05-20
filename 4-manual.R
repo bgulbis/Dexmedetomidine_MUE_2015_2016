7e5323d2f6693e054470bc1bcc6928b16a0242d6
bgulbis <brian.gulbis@gmail.com>
2016-05-20 14:02:43 -0500

    update manual data export

--- 4-manual.R
+++ 4-manual.R
@@ -4,9 +4,9 @@ source("0-library.R")
 
 tmp <- get_rds(dir.save)
 
-tmp.identify <- read_edw_data(dir.data, "identifiers", "id")
-
-tmp.manual <- select(tmp.identify, fin)
+tmp.manual <- read_edw_data(dir.data, "identifiers", "id") %>%
+    inner_join(data.dexmed.first, by = "pie.id") %>%
+    select(fin, location, arrive.datetime, start.datetime)
 
 library(readr)
 write_csv(tmp.manual, paste(dir.save, "manual.csv", sep = "/"))
